96ab74b64f0735f5b93868f8f8bfe4b8b0fca92d
Harold <pxduharold@126.com>
2017-09-05 16:11:50 +0800

    store data into mysql

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from flask import Flask, render_template
 from flask_mqtt import Mqtt
 from flask_socketio import SocketIO
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+import json
+
 
 eventlet.monkey_patch()
 
@@ -25,6 +28,7 @@ app.config['MQTT_TLS_ENABLED'] = False
 app.config['MQTT_LAST_WILL_TOPIC'] = 'home/lastwill'
 app.config['MQTT_LAST_WILL_MESSAGE'] = 'bye'
 app.config['MQTT_LAST_WILL_QOS'] = 2
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mqtt_test:123456@127.0.0.1/mqtt_test'
 
 # Parameters for SSL enabled
 # app.config['MQTT_BROKER_PORT'] = 8883
@@ -35,6 +39,21 @@ app.config['MQTT_LAST_WILL_QOS'] = 2
 mqtt = Mqtt(app)
 socketio = SocketIO(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+
+class mqtt_data(db.Model):
+    __tablename__ = 'mqtt_data'
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String(64))
+    message = db.Column(db.String(128))
+
+    def __init__(self, topic, message):
+        self.topic = topic
+        self.message = message
+
+    def __repr__(self):
+        return '<mqtt_data %r>' % self.topic + ':' + self.message
 
 
 @app.route('/')
@@ -60,6 +79,14 @@ def handle_mqtt_message(client, userdata, message):
         topic=message.topic,
         payload=message.payload.decode()
     )
+    d = mqtt_data(topic=data['topic'], message=data['payload'])
+    db.session.add(d)
+    db.session.commit()
+    #print(d)
+    p = message.payload.decode()
+    json_acceptable_string = p.replace("'", "\"")
+    p = json.loads(json_acceptable_string)
+    print(p['temperature'])
     socketio.emit('mqtt_message', data=data)
 
 
@@ -69,4 +96,5 @@ def handle_logging(client, userdata, level, buf):
 
 
 if __name__ == '__main__':
+    db.create_all()
     socketio.run(app, host='0.0.0.0', port=5000, use_reloader=True, debug=True)
